3ae8efdc49cde50363553f646bd2eba597072290
RiwazJoshi <devriwazjoshi@gmail.com>
2022-11-29 16:00:45 +0545

    htmlandCssfiles

---
+++ Java/form.js
@@ -0,0 +1,11 @@
+
+function callvalue(){
+   var name= document.getElementById("Username").value;
+    var Password= document.getElementById("password").value;
+    var E-mail= document.getElementById("email").value;
+   var Date of brith= document.getElementById("Dob").value;
+    Document.writeln("Your info:" + "<br>"+ "your name:" +name +"<br>");
+    Document.writeln("your Password:" +Password +"<br>");
+    Document.writeln("your Email:" +E-mail +"<br>");
+ Document.writeln("your Date OF Birth:" +Date of birth +"<br>");
+}
