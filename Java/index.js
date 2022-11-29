10603d330c96d117030cc19d1ddd0a553bd8a4e4
RiwazJoshi <devriwazjoshi@gmail.com>
2022-11-29 23:47:16 +0545

    Java

---
+++ Java/index.js
@@ -0,0 +1,12 @@
+
+
+container ="bunch of items"
+brand = "SAMSUNG"
+var name="john doe"
+const pi = 3.17
+console.log(brand)
+console.log(container)
+console.log(name)
+console.log(pi)
+let course="mern"; 
+console.log(course)
